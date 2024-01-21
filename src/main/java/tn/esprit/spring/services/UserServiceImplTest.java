d88387300a35cb4db6a601dfa584b51c04602b20
Phenix0103 <61549263+Phenix0103@users.noreply.github.com>
2024-01-21 21:00:50 +0100

    Update UserServiceImplTest.java

--- src/main/java/tn/esprit/spring/services/UserServiceImplTest.java
+++ src/main/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -5,14 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
-class UserServiceImplTest{
-	@AutowiredIUserService us; 
-	@Test
-	@Order(1)
-	public voidtestRetrieveAllUsers()  
-	{List<User> listUsers= us.retrieveAllUsers(); 
-	 Assertions.assertEquals(0, listUsers.size());
-	} 
+class UserServiceImplTest {
+
+    @Autowired
+    IUserService us;
+
+    @Test
+    @Order(1)
+    public void testRetrieveAllUsers() {
+        List<User> listUsers = us.retrieveAllUsers();
+        Assertions.assertEquals(0, listUsers.size());
+    }
 }
